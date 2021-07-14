cef578728b9e64b1cc306522353e67c24aa8dd3d
Syver Døving Agdestein <syverda@icloud.com>
2021-07-14 18:42:10 +0200

    Fix advection equation

--- src/equations/solve_advection.jl
+++ src/equations/solve_advection.jl
@@ -1,5 +1,5 @@
 function solve(
-    equation::AdvectionEquation{PeriodicIntervalDomain,TopHatFilter},
+    equation::AdvectionEquation{PeriodicIntervalDomain{T},F},
     u,
     tlist,
     n;
@@ -7,35 +7,35 @@ function solve(
     solver = QNDF(),
     abstol = 1e-4,
     reltol = 1e-3,
-)
+) where {T,F}
 
     @unpack domain, filter = equation
 
     x = discretize(domain, n)
     Δx = x[2] - x[1]
 
-    h = filter.width
-    dh(x) = 0.0
-    α(x) = 1 / 3 * dh(x) * h(x)
-
-    ## Get matrices
+    # Get matrices
     C = advection_matrix(domain, n)
-    # W = filter_matrix(filter, domain, n)
-    # R = inverse_filter_matrix(filter, domain, n)
-    W = filter_matrix_meshwidth(filter, domain, n)
-    R = inverse_filter_matrix_meshwidth(filter, domain, n)
-    A = spdiagm(α.(x))
+    D = diffusion_matrix(domain, n)
+    W = filter_matrix(filter, domain, n)
+    R = inverse_filter_matrix(filter, domain, n)
 
     uₕ = u.(x)
     ūₕ = W * uₕ
 
     du!(du, u, p, t) = mul!(du, p.J, u)
     if method == "filterfirst"
+        F === TopHatFilter ||
+            error("Method \"filterfirst\" is only implemented for TopHatFilter")
+        h = filter.width
+        dh(x) = 0.0
+        α(x) = 1 / 3 * dh(x) * h(x)
+        A = spdiagm(α.(x))
         p = (; J = -C + A * D)
         odefunction =
             ODEFunction(du!, jac = (J, u, p, t) -> (J .= p.J), jac_prototype = p.J)
         problem = ODEProblem(odefunction, ūₕ, tlist, p)
-        sol = OrdinaryDiffEq.solve(problem, solver; abstol, reltol)
+        solution = OrdinaryDiffEq.solve(problem, solver; abstol, reltol)
     elseif method == "discretizefirst"
         p = (; J = -W * C * R)
         odefunction = ODEFunction(

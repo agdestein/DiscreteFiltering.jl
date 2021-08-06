92b3b86d8b1d5ccffb733afbd68e8814a4989a10
Syver Døving Agdestein <40632532+agdestein@users.noreply.github.com>
2021-08-06 13:27:58 +0200

    Abstract methods

--- src/equations/solve.jl
+++ src/equations/solve.jl
@@ -1,22 +1,9 @@
 """
-    solve(equation, u, tlist, n; method = "filterfirst")
+    solve
 
-Solve `equation` from `tlist[1]` to `tlist[2]` with initial conditions `u` and a
-discretization of `n` points. If `method` is `"filterfirst"`, the equation is filtered then
-discretized. If `method` is `"discretizefirst"`, the equation is discretized then filtered.
+Solve equation.
 """
-function solve(
-    equation::Equation,
-    u,
-    tlist,
-    n;
-    method = "filterfirst",
-    solver = QNDF(),
-    abstol = 1e-4,
-    reltol = 1e-3,
-)
-    error("Not implemented")
-end
+function solve end
 
 include("solve_advection.jl")
 include("solve_diffusion.jl")

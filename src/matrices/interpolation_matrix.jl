1b4bb483529a5821c6fd35bc2dba647e0b0d6f14
Syver Døving Agdestein <syverda@icloud.com>
2021-10-11 07:21:38 +0200

    Fix edge case

--- src/matrices/interpolation_matrix.jl
+++ src/matrices/interpolation_matrix.jl
@@ -9,8 +9,10 @@ function interpolation_matrix(x, ξ)
     n = ξᵀ[[1], 1:end-1] .≤ x .< ξᵀ[[1], 2:end]
     for m = 1:length(x)-1
         nₘ = findfirst(n[m, :])
-        A[m, nₘ] = (ξᵀ[nₘ+1] - x[m]) / (ξᵀ[nₘ+1] - ξᵀ[nₘ])
-        A[m, nₘ+1] = (x[m] - ξᵀ[nₘ]) / (ξᵀ[nₘ+1] - ξᵀ[nₘ])
+        if !isnothing(nₘ)
+            A[m, nₘ] = (ξᵀ[nₘ+1] - x[m]) / (ξᵀ[nₘ+1] - ξᵀ[nₘ])
+            A[m, nₘ+1] = (x[m] - ξᵀ[nₘ]) / (ξᵀ[nₘ+1] - ξᵀ[nₘ])
+        end 
     end
     A[x.<ξ[1], 1] .= 1
     A[ξ[end].≤x, end] .= 1
